4487350bb7e5461c4289aed2515e4cd42071ec1c
zubairadams <zubair.adams720@gmail.com>
2018-03-22 12:18:22 +0200

    changes

--- findItemsOver.js
+++ findItemsOver.js
@@ -1,5 +1,5 @@
 function findItemsOver(item,amount){
-
+var amount = 20; 
   var man = [];
   for(var i = 0; i < item.length; i++){
   var theitem = item[i];
--- tests/findItemsOver.tests.js
+++ tests/findItemsOver.tests.js
@@ -11,11 +11,21 @@ describe('the findItemsOver function', function(){
     {name : 'apples', qty : 10},
     {name : 'pears', qty : 50},
     {name : 'bananas', qty : 27},
-    {name : 'apples', qty : 3},
+    {name : 'apples', qty : 30},
 ];
 
-  it('should return amount of items over 20', function(){
-    assert.deepEqual(findItemsOver(itemList),2);
+  it('should return three items over 20', function(){
+    assert.deepEqual(findItemsOver(itemList),3);
+  })
+
+  var itemList2 = [
+    {name : 'apples', qty : 10},
+    {name : 'pears', qty : 5},
+    {name : 'bananas', qty : 7},
+    {name : 'apples', qty : 3},
+];
+  it('should return no items over 20 ', function(){
+    assert.deepEqual(findItemsOver(itemList2),0)
   })
 
 })
