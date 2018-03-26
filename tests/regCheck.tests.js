3a8cc0d7491c2f74881d958ce4f471c1310b6a2c
zubairadams <zubair.adams720@gmail.com>
2018-03-26 14:35:05 +0200

    new

--- tests/regCheck.tests.js
+++ tests/regCheck.tests.js
@@ -1,11 +1,11 @@
 describe('the regCheck function', function(){
 
   it('should confirm location is bellville', function(){
-    assert.equal(regCheck('787894 CA','CA'),true);
+    assert.equal(regCheck('DV 23 NB GP','GP'),true);
   });
   it('should deny location is bellville', function(){
-    assert.equal(regCheck('CS 895541','CA'),false);
+    assert.equal(regCheck('MP 895541','CA'),false);
   });
 
 
-});
+})
